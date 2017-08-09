54fb7636b4f9c8fb693df8a603d65d7fcaead3b2
Sasha Friedenberg <sashafriedenberg@gmail.com>
2017-08-09 13:46:25 -0400

    switch STDOUT to be injectable

--- lib/re.rb
+++ lib/re.rb
@@ -6,21 +6,20 @@ require "re/graph_putter"
 
 module Re
   class << self
-    def run(args, input_stream = STDIN, exec_type = Options::ExecType::MODULE)
+    def run(args, input_stream = STDIN, output_stream = STDOUT, exec_type = Options::ExecType::MODULE)
       begin
         options = Options.new(args)
       rescue ArgumentError => e
-        puts e
+        output_stream.puts e
         exit(1)
       end
 
-      putter = TabGraphPutter.new(options, STDOUT)
+      putter = TabGraphPutter.new(options, output_stream)
 
       visitor = Graph::Visitor.new(
         options,
         input_stream,
       )
-
 
       lobby_nodes = visitor.traverse do |node|
         putter.print_node(node)
